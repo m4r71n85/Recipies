355f403b5aa288e6b1feabd18c4a262e6627c371
m4r71n85 <martin85@abv.bg>
2013-08-14 16:14:36 +0300

    Merge branch 'master' of https://github.com/m4r71n85/Recipies (reverse-merged from commit d36808eb274258ba5b8b14ca9d311327c9cc666e)

---
+++ Recipies/Recipies.Repository/DbUsersRepository.cs
@@ -0,0 +1,48 @@
+﻿using Reciepes.Data;
+using Recipies.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipies.Repository
+{
+	public class DbUsersRepository : IRepository<User>
+	{
+		private RecipieContext dbContext;
+
+		public DbUsersRepository(RecipieContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public User Add(User item)
+		{
+			User returnedUser = this.dbContext.Users.Add(item);
+			this.dbContext.SaveChanges();
+			return returnedUser;
+		}
+
+		public void Update(int id, User item)
+		{
+			this.dbContext.Entry(item).State = EntityState.Modified;
+		}
+
+		public void Remove(int id)
+		{
+			this.dbContext.Users.Remove(this.dbContext.Users.Find(id));
+		}
+
+		public User GetById(int id)
+		{
+			return this.dbContext.Users.Where(x => x.UserID == id).Select(x => x).FirstOrDefault();
+		}
+
+		public IQueryable<User> GetAll()
+		{
+			return this.dbContext.Users;
+		}
+	}
+}
---
+++ Recipies/Recipies.Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Recipies.Repository
+{
+	public interface IRepository<T>
+	{
+		T Add(T item);
+		void Update(int id, T item);
+		void Remove(int id);
+		T GetById(int id);
+		IQueryable<T> GetAll();
+	}
+}
