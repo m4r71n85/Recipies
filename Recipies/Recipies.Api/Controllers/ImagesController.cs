13cd550a1bd3c00cb6af539f3dfdbadd4520107f
Ivaylo Ivanov Kirilov <ivailokirilov93@gmail.com>
2013-08-15 21:51:03 +0300

    image to server upload

---
+++ Recipies/Recipies.Api/Controllers/ImagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Recipies.Api.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        public void UploadImage(string sessionKey)
+        {
+            HttpContext current = HttpContext.Current;
+            for (int i = 0; i < current.Request.Files.Count; i++)
+            {
+                HttpPostedFile file = current.Request.Files[i];
+                Stream stream = file.InputStream;
+
+                using (var fileStream = File.Create("~/App_Data/" + file.FileName))
+                {
+                    stream.CopyTo(fileStream);
+                }  
+            }
+        }
+    }
+}
